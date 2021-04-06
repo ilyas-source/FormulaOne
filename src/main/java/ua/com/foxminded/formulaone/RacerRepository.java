88149444c422f3d2ec0c037382af0c94b9ca41e6
isaitov <saitov1981@gmail.com>
2021-04-06 14:59:33 +0500

    RacerRepository GetRacers change

--- src/main/java/ua/com/foxminded/formulaone/RacerRepository.java
+++ src/main/java/ua/com/foxminded/formulaone/RacerRepository.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import static java.util.stream.Collectors.*;
@@ -15,22 +14,15 @@ public class RacerRepository {
 
 	public List<Racer> getRacers(String startLogFileName, String endLogFileName, String abbrFileName)
 			throws IOException {
-		List<String> abbreviations = Files.readAllLines(Paths.get(abbrFileName));
 		List<String> startTimes = Files.readAllLines(Paths.get(startLogFileName));
 		List<String> endTimes = Files.readAllLines(Paths.get(endLogFileName));
 
-		List<Racer> parsedRacers = abbreviations.stream().map(this::createRacerFromString).collect(toList());
-
-		parsedRacers.forEach(racer -> {
+		return Files.readAllLines(Paths.get(abbrFileName)).stream().map(this::createRacerFromString).peek(racer -> {
 			LocalDateTime startTime = parseLapInfo(startTimes, racer);
 			LocalDateTime endTime = parseLapInfo(endTimes, racer);
-
 			Duration lapDuration = Duration.between(startTime, endTime);
-
 			racer.setBestLapTime(lapDuration);
-		});
-
-		return parsedRacers;
+		}).collect(toList());
 	}
 
 	public Racer createRacerFromString(String string) {
@@ -38,13 +30,12 @@ public class RacerRepository {
 		return new Racer(params[0], params[1], params[2]);
 	}
 
-	public LocalDateTime parseLapInfo(List<String> list, Racer racer) {
+	private LocalDateTime parseLapInfo(List<String> list, Racer racer) {
 		return parseTimeDateFromString(list.stream().filter(line -> line.startsWith(racer.getAbbreviation())).findAny()
 				.orElseThrow(() -> new NoSuchElementException("No such element")).substring(3));
 	}
 
-	public LocalDateTime parseTimeDateFromString(String dateTime) {
+	private LocalDateTime parseTimeDateFromString(String dateTime) {
 		return LocalDateTime.parse(dateTime, DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS"));
 	}
-
 }
